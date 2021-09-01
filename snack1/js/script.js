b94d2bb9f5395e30f40c504c08fc43c825bd7cfc
ALAM-97 <alexamara97@gmail.com>
2021-09-01 10:45:15 +0200

    FIX - JS Snack 1

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,21 +1,26 @@
 // 1. chiedere all’utente di inserire una parola
 // 2. creare una funzione per capire se la parola inserita è palindroma
-function revString(string) {
-    return string.split("").reverse().join("");
-}
 
 function polindromeVer(string) {
-    if (string == revString(string)) {
-        return document.getElementById("esito").innerHTML = "Le parole sono palindrome";
+
+    var revString = string.split("").reverse().join("");
+
+    if (string == revString) {
+        return true; 
     }
-    return document.getElementById("esito").innerHTML = "Le parole NON sono palindrome";
+    return false; 
 }
 
 // 1.
 var userInput = prompt("inserisci una parola");
 document.getElementById("user-input").innerHTML = userInput;
-var reverseWord = revString(userInput);
 
 // 2.
-var wordVerifier = polindromeVer(userInput);
-document.getElementById("rev-word").innerHTML = reverseWord;
+var reverseWord = polindromeVer(userInput);
+document.getElementById("rev-word").innerHTML = userInput.split("").reverse().join("");;
+
+if (polindromeVer(userInput) == true) {
+    document.getElementById("esito").innerHTML = "Le parole sono palindrome";
+} else {
+    document.getElementById("esito").innerHTML = "Le parole NON sono palindrome";
+}
